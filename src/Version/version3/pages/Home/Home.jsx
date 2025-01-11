e5980de48bf49ca25ea53466f9b4fbd779ba87c7
ArifMiah07 <arifmiah.me322@gmail.com>
2025-01-11 09:05:37 +0600

    update--<home>--

--- src/Version/version3/pages/Home/Home.jsx
+++ src/Version/version3/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-// import { ScrollContainer } from "react-nice-scroll";
+import { ScrollContainer } from "react-nice-scroll";
 import "react-nice-scroll/dist/styles.css";
 import LeftSideBar from "../../components/LeftSideBar/LeftSideBar";
 // import { InteractiveCursor } from "../../../../components/cursor/CursorDemo";
@@ -10,53 +10,104 @@ const Home = () => {
       {/* <InteractiveCursor></InteractiveCursor> */}
       <section className="lg:col-span-3 lg:w-[330px] bg-[#1B8B09] p-5 z-50 lg:z-0">
         <div className="lg:w-[270px] bg-[#60A15680] ">
-            <LeftSideBar></LeftSideBar>
+          <LeftSideBar></LeftSideBar>
         </div>
       </section>
       {/* right */}
       <section className="lg:col-span-9 w-full  bg-[#FF0088] p-1 z-10 lg:z-0">
-        {/* <ScrollContainer> */}
-          <div className="bg-[#D663A080] lg:w-[930px]  p-1  lg:mb-2 ">
-            <div className="p-5 bg-[#D9D9D9] flex flex-col items-center gap-3 lg:mb-2 ">
-              <div className="bg-[#D9D5D5] flex flex-col items-center w-full p-2">
-                  <h1 className="text-[36px] ">Hi, I’m Arif Miah</h1>
-                  <p className="text-[24px] "> I&apos;m a MERN Stack-focused Full Stack Web Developer</p>
-              </div>
-              <div className="bg-[#D9D5D5] flex flex-col items-center w-full p-2">
-                  <div>
-                    <h1 className="text-[32px] ">Skills</h1>
+        <ScrollContainer>
+        <div className="bg-[#D663A080] lg:w-[930px]  p-1  lg:mb-2 ">
+          <div className="p-5 bg-[#D9D9D9] flex flex-col items-center gap-3 lg:mb-2 ">
+            {/* intro */}
+            <div className="bg-[#D9D5D5] flex flex-col items-center w-full p-2">
+              <h1 className="text-[36px] ">Hi, I’m Arif Miah</h1>
+              <p className="text-[24px] ">
+                {" "}
+                I&apos;m a MERN Stack-focused Full Stack Web Developer
+              </p>
+            </div>
+            {/* skills */}
+            <div className="bg-[#D9D5D5]  flex flex-col items-center justify-end w-full">
+              <div className="flex flex-col items-center w-full p-2">
+                <div className="flex items-center">
+                  <div className="flex items-center justify-center">
+                    <div className="w-[200px] h-[2px] bg-black "></div>
+                    <div className="w-4 h-4 border-b-2 border-l-2 border-current rounded-sm rotate-45"></div>
                   </div>
-                  <div>
-                    <p className="text-[16px] ">HTML, CSS, JavaScript, TypeScript, Node JS, Express, MongoDB, , Firebase, JWT, 
-                    C/C++, Python, English, Spanish ...</p>
+                  <h1 className="text-[32px] mx-3 ">Skills</h1>
+                  <div className=" flex items-center justify-center rotate-180">
+                    <div className="w-[200px] h-[2px] bg-black "></div>
+                    <div className="w-4 h-4 border-b-2 border-l-2 border-current rounded-sm rotate-45"></div>
                   </div>
-              </div>
-              <div className="bg-[#D9D5D5] flex flex-col items-center w-full p-2">
-              <div>
-                    <h1 className="text-[32px] ">Experience</h1>
-                  </div>
-                  <div>
-                    <p className="text-[16px] ">Fresher but 1 years of solid Learning & Problem Solving Experience</p>
-                  </div>
-              </div>
-              <div className="bg-[#D9D5D5] flex flex-col items-center w-full p-2">
-              <div>
-                    <h1 className="text-[32px] ">Contact</h1>
-                  </div>
-                  <div>
-                    <p className="text-[16px] text-center ">Gmail: arifmiah101@gmail.com</p>
-                    <p className="text-[16px] text-center ">X: ArifMiah01</p>
-                  </div>
-              </div>
-              <div className="bg-[#D9D5D5] flex flex-row items-center justify-center gap-4 w-full p-2">
-                  <div className="w-[24px] h-[24px] rounded-full bg-red-300"></div>
-                  <div className="w-[24px] h-[24px] rounded-full bg-red-300"></div>
-                  <div className="w-[24px] h-[24px] rounded-full bg-red-300"></div>
-                  <div className="w-[24px] h-[24px] rounded-full bg-red-300"></div>
+                </div>
+                <div>
+                  <p className="text-[16px] ">
+                    HTML, CSS, JavaScript, TypeScript, Node JS, Express,
+                    MongoDB, , Firebase, JWT, C/C++, Python, English, Spanish
+                    ...
+                  </p>
+                </div>
+                {/* border div */}
+                <div className="hover:w-full w-[100px] h-[2px] bg-slate-600 mt-4"></div>
               </div>
             </div>
+            {/* experience */}
+            <div className="bg-[#D9D5D5] flex flex-col items-center w-full p-2">
+              <div>
+                <div className="flex items-center">
+                  <div className="flex items-center justify-center">
+                    <div className="w-[200px] h-[2px] bg-black "></div>
+                    <div className="w-4 h-4 border-b-2 border-l-2 border-current rounded-sm rotate-45"></div>
+                  </div>
+                  <h1 className="text-[32px] mx-3 ">Experience</h1>
+                  <div className=" flex items-center justify-center rotate-180">
+                    <div className="w-[200px] h-[2px] bg-black "></div>
+                    <div className="w-4 h-4 border-b-2 border-l-2 border-current rounded-sm rotate-45"></div>
+                  </div>
+                </div>
+                <div>
+                  <p className="text-[16px] text-center ">
+                    Fresher but 1 years of solid Learning & Problem Solving
+                    Experience
+                  </p>
+                </div>
+              </div>
+              {/* border div */}
+              <div className="hover:w-full w-[100px] h-[2px] bg-slate-600 mt-4"></div>
+            </div>
+            {/* contact */}
+            <div className="bg-[#D9D5D5] flex flex-col items-center w-full p-2">
+              <div className="flex flex-col items-center w-full p-2">
+                <div className="flex items-center">
+                  <div className="flex items-center justify-center">
+                    <div className="w-[200px] h-[2px] bg-black "></div>
+                    <div className="w-4 h-4 border-b-2 border-l-2 border-current rounded-sm rotate-45"></div>
+                  </div>
+                  <h1 className="text-[32px] ">Contact</h1>
+                  <div className=" flex items-center justify-center rotate-180">
+                    <div className="w-[200px] h-[2px] bg-black "></div>
+                    <div className="w-4 h-4 border-b-2 border-l-2 border-current rounded-sm rotate-45"></div>
+                  </div>
+                </div>
+                <div>
+                  <p className="text-[16px] text-center ">
+                    Gmail: arifmiah101@gmail.com
+                  </p>
+                  <p className="text-[16px] text-center ">X: ArifMiah01</p>
+                </div>
+              </div>
+              {/* border div */}
+              <div className="hover:w-full w-[100px] h-[2px] bg-slate-600 mt-4"></div>
+            </div>
+            <div className="bg-[#D9D5D5] flex flex-row items-center justify-center gap-4 w-full p-2">
+              <div className="w-[24px] h-[24px] rounded-full bg-red-300"></div>
+              <div className="w-[24px] h-[24px] rounded-full bg-red-300"></div>
+              <div className="w-[24px] h-[24px] rounded-full bg-red-300"></div>
+              <div className="w-[24px] h-[24px] rounded-full bg-red-300"></div>
+            </div>
           </div>
-        {/* </ScrollContainer> */}
+        </div>
+        </ScrollContainer>
       </section>
     </div>
   );
