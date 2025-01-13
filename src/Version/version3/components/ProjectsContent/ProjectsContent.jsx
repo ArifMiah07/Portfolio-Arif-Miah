70a8aa898abeb7b5e6bffca14b9b23949f33f038
ArifMiah07 <arifmiah.me322@gmail.com>
2025-01-13 09:58:04 +0600

    updt

--- src/Version/version3/components/ProjectsContent/ProjectsContent.jsx
+++ src/Version/version3/components/ProjectsContent/ProjectsContent.jsx
@@ -1,6 +1,7 @@
-const ProjectsContent = () => {
-  const img =
-    "https://images.pexels.com/photos/270637/pexels-photo-270637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+// eslint-disable-next-line react/prop-types
+const ProjectsContent = ({img, name, overview, tech}) => {
+  // const img =
+  //   "https://images.pexels.com/photos/270637/pexels-photo-270637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
 
   return (
     <div className="mx-auto w-full h-full p-5 lg:p-5 gap-5 bg-[#D9D9D9] flex flex-col ">
@@ -10,19 +11,24 @@ const ProjectsContent = () => {
           <img
             className=" object-cover transition-opacity duration-300 ease-in-out group-hover:opacity-50"
             src={img}
-            alt="Project"
+            alt="Project image"
           />
         </div>
 
         {/* Hover Details */}
         <div className="absolute inset-0 bg-white bg-opacity-80 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 ease-in-out">
           <div className="flex flex-col justify-center items-center h-full opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out">
-            <h1 className="text-lg font-bold">img.name</h1>
-            <h2 className="text-md text-gray-600">img.price</h2>
-            <p className="text-center p-2 mt-2">img.description</p>
+            <h1 className="text-lg font-bold">Name : {name} </h1>
+            <h2 className="text-md text-gray-600">Overview : {overview} </h2>
+            <p className="text-center p-2 mt-2">Technology : {tech} </p>
+            <div>
             <button className="text-black hover:text-white border hover:bg-gray-600 p-3 ">
-              View full image
+              Live Site
             </button>
+            <button className="text-black hover:text-white border hover:bg-gray-600 p-3 ">
+              Github Repo
+            </button>
+            </div>
           </div>
         </div>
       </div>
--- src/Version/version3/pages/Projects/Projects.jsx
+++ src/Version/version3/pages/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import LeftSideBar from "../../components/LeftSideBar/LeftSideBar";
 import { ScrollContainer } from "react-nice-scroll";
 import "react-nice-scroll/dist/styles.css";
 import ProjectsContent from "../../components/ProjectsContent/ProjectsContent";
+import img1 from '../../../../../public/assets/Desktop3.png'
 
 const Projects = () => {
   return (
@@ -16,10 +17,10 @@ const Projects = () => {
       <section className="lg:col-span-9 w-full bg-[#FF0088] p-2 lg:p-1 flex items-center">
         <ScrollContainer>
           <div className="bg-[#D663A080] w-full shadow-lg drop-shadow-md p-1  lg:mb-2">
-            <ProjectsContent />
-            <ProjectsContent />
-            <ProjectsContent />
-            <ProjectsContent />
+            <ProjectsContent img={img1} name={'Artymia'} overview={'Artymia is a art selling Website'} tech={'HTML, CSS, SCSS, Tailwind \n css, React Js, Node js, Stripe, JWT, Firebase'} />
+            <ProjectsContent img={img1} name={'Smart Kidz'} overview={'Artymia is a art selling Website'} tech={'HTML, CSS, SCSS, Tailwind \n css, React Js, Node js, Stripe, JWT, Firebase'} />
+            <ProjectsContent img={img1} name={'XFNF'} overview={'Artymia is a art selling Website'} tech={'HTML, CSS, SCSS, Tailwind \n css, React Js, Node js, Stripe, JWT, Firebase'} />
+            <ProjectsContent img={img1} name={'Clyst'} overview={'Artymia is a art selling Website'} tech={'HTML, CSS, SCSS, Tailwind \n css, React Js, Node js, Stripe, JWT, Firebase'} />
           </div>
         </ScrollContainer>
       </section>
