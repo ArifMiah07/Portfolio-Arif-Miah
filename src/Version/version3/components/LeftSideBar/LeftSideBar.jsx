6c3b6d83f7f05b55436c80215cf26efb25341a2e
ArifMiah07 <arifmiah.me322@gmail.com>
2025-04-02 22:22:47 +0600

    updt

--- src/Version/version3/components/LeftSideBar/LeftSideBar.jsx
+++ src/Version/version3/components/LeftSideBar/LeftSideBar.jsx
@@ -1,242 +1,3 @@
-// import profileImg from "../../../../../public/assets/profile-removebg-preview.png";
-// import { Link } from "react-router-dom";
-// import { FaAngleDoubleRight, FaGithub } from "react-icons/fa";
-// import { FaLinkedin, FaXTwitter } from "react-icons/fa6";
-// import { MdEmail } from "react-icons/md";
-// import { TbFileCv } from "react-icons/tb";
-// import { FaAngleDoubleLeft } from "react-icons/fa";
-// import { useState } from "react";
-// import { GiNightSky } from "react-icons/gi";
-// import { CiBrightnessDown } from "react-icons/ci";
-// import { IoSettings } from "react-icons/io5";
-// import { IoSettingsOutline } from "react-icons/io5";
-// import { CiStar } from "react-icons/ci";
-// import { FaStar } from "react-icons/fa";
-// import { PiEyes } from "react-icons/pi";
-// // import { TbMenuOrder } from "react-icons/tb";
-// import { CgMenuLeft } from "react-icons/cg";
-// import { IoMenu } from "react-icons/io5";
-// // import { SiGmail } from 'react-icons/si';
-// // import Dropdown from '../Dropdown/Dropdown';
-
-// const LeftSideBar = () => {
-//   const [sideBarClose, setSideBarClose] = useState(false);
-//   const [nightModeClose, setNightModeClose] = useState(false);
-//   const [settingModeClose, setSettingModeClose] = useState(false);
-//   const [startClose, setStartClose] = useState(false);
-//   let [countStar, setCountStar] = useState(1);
-//   const [view, setView] = useState(false);
-//   // const [viewEyes, setViewEyes] = useState(10);
-//   const [navbarClose, setNavbarClose] = useState(false);
-
-//   const viewEyes = "1.4k";
-
-//   const handleNavbarTToggle = () => {
-//     // console.log("clicked");
-//     setNavbarClose(!navbarClose);
-//   };
-
-//   const handleView = () => {
-//     // console.log("hovered");
-//     setView(!view);
-//   };
-
-//   const handleStar = () => {
-//     // console.log("hello");
-//     setStartClose(!startClose);
-//     setCountStar(countStar + 1);
-//     if( countStar === 11){
-//       setCountStar(countStar = 0)
-//     }
-//     // console.log(countStar);
-//   };
-
-//   const handleSettingMode = () => {
-//     // console.log("clicked");
-//     setSettingModeClose(!settingModeClose);
-//   };
-
-//   const handleSideBar = () => {
-//     // console.log("clicked");
-//     setSideBarClose(!sideBarClose);
-//   };
-//   const handleNightMode = () => {
-//     console.log("clicked");
-//     setNightModeClose(!nightModeClose);
-//   };
-//   // console.log(viewEyes);
-
-//   const navLinksStyle =
-//     "border-b-2 border-green-800 hover:p-1 hover:bg-green-200 hover:shadow-2xl rounded-md hover:text-center delay-75 hover:transition-shadow transition-all hover:border-x-2 hover:border-green-600 hover:drop-shadow-sm ";
-
-//   const navLinks = (
-//     <>
-//       <li className={navLinksStyle}>
-//         <Link to={"/v3/"}>Home</Link>
-//       </li>
-//       <li className={navLinksStyle}>
-//         <Link to={"/v3/about"}>About</Link>
-//       </li>
-//       <li className={navLinksStyle}>
-//         <Link to={"/v3/skills"}>Skills</Link>
-//       </li>
-//       <li className={navLinksStyle}>
-//         <Link to={"/v3/projects"}>Projects</Link>
-//       </li>
-//       <li className={navLinksStyle}>
-//         <Link to={"/v3/blogs"}>Blogs</Link>
-//       </li>
-//       <li className={navLinksStyle}>
-//         <Link to={"/v3/contact"}>Contact</Link>
-//       </li>
-//     </>
-//   );
-
-//   const smLinksStyle =
-//     "text-2xl hover:text-3xl hover:text-[#384ba7] transition-all duration-300 ease-in-out";
-//   const smLinks = (
-//     <>
-//       <FaGithub className={smLinksStyle} />
-//       <FaLinkedin className={smLinksStyle} />
-//       <MdEmail className={smLinksStyle} />
-//       <FaXTwitter className={smLinksStyle} />
-//       <TbFileCv className={smLinksStyle} />
-//     </>
-//   );
-
-//   const interactionIconsStyle =
-//     "flex items-center justify-center bg-green-50 shadow-xl drop-shadow-xl rounded-full w-10 h-10 ";
-
-//   const applyCustomizationStyle =
-//     "border-b-2 border-green-700 hover:border-b-2 hover:border-green-300 hover:p-2 hover:text-center cursor-pointer hover:bg-blue-700 hover:transition-all hover:rounded-md";
-
-//   return (
-//     <div
-//       className={`bg-[#60A15680] p-1 lg:p-5 flex sm:flex-row sm:justify-between sm:items-center lg:flex-col lg:gap-3  `}>
-//       <div className="flex justify-center items-center   ">
-//         <img
-//           className="shadow-xl drop-shadow-xl rounded-full w-[40px] md:w-[120px] lg:w-[160px] xl:w-[180px] xl:h-[160.28]  "
-//           src={profileImg}
-//           alt="profile image of arif miah"
-//         />
-//       </div>
-//       {/* settings / users interactions */}
-//       <div className="flex flex-row justify-between items-center gap-1 lg:justify-evenly lg:mt-2">
-//         {/* sidebar folding / Drawer */}
-//         <div onClick={handleSideBar} className={interactionIconsStyle}>
-//           {sideBarClose ? <FaAngleDoubleRight /> : <FaAngleDoubleLeft />}
-//         </div>
-//         {/* night mode toggle / theme */}
-//         <div onClick={handleNightMode} className={interactionIconsStyle}>
-//           {nightModeClose ? <CiBrightnessDown /> : <GiNightSky />}
-//         </div>
-//         {/* apply settings */}
-//         <div onClick={handleSettingMode} className={interactionIconsStyle}>
-//           {settingModeClose ? (
-//             <div className="relative">
-//               {" "}
-//               <IoSettings className="" />{" "}
-//               <div className="absolute top-50 right-50 bg-sky-300 p-5 rounded-xl ">
-//                 {" "}
-//                 <h1 className="text-center">Customization Setting</h1>
-//                 <div>
-//                   <ul className="border border-red-400 bg-sky-200 rounded-lg p-1">
-//                     <li className={applyCustomizationStyle}>Font Size</li>
-//                     <li className={applyCustomizationStyle}>Front Family</li>
-//                     <li className={applyCustomizationStyle}>Color</li>
-//                     <li className={applyCustomizationStyle}>Mode</li>
-//                   </ul>
-//                 </div>
-//               </div>
-//             </div>
-//           ) : (
-//             <div>
-//               {" "}
-//               <IoSettingsOutline />{" "}
-//             </div>
-//           )}
-//         </div>
-//         {/* give star  */}
-//         <div
-//           onClick={handleStar}
-//           className={`flex items-center justify-center bg-green-50 shadow-xl drop-shadow-xl rounded-full  w-10 h-10  `}>
-//           {startClose ? (
-//             <FaStar
-//               className={`${
-//                 countStar === 2
-//                   ? "w-10 h-10 text-yellow-300 "
-//                   : countStar === 4
-//                   ? "w-12 h-12 text-pink-600"
-//                   : countStar === 6
-//                   ? "w-14 h-14 text-blue-600"
-//                   : countStar === 8
-//                   ? "w-16 h-16 text-[#26c2a3]"
-//                   : countStar === 10
-//                   ? "w-16 h-16 text-[#26c25d]"
-//                   : ""
-//               }`}
-//             />
-//           ) : (
-//             <CiStar className="" />
-//           )}
-//         </div>
-//         {/* show website view */}
-//         <div
-//           onMouseEnter={handleView}
-//           className={`flex items-center justify-center bg-green-50 shadow-xl drop-shadow-xl rounded-full w-10 h-10 text-[15px] hover:text-[14px] hover:transition-all hover:delay-75 `}>
-//           {view ? viewEyes : <PiEyes />}
-//         </div>
-//       </div>
-//       {/* footer */}
-//       <div>
-//         {/* sm */}
-//         <div onClick={handleNavbarTToggle} className="lg:hidden">
-//           {navbarClose ? (
-//             <div className={interactionIconsStyle}>
-//               <CgMenuLeft />
-
-//               <div className="relative top-6">
-//                 <div className=" flex flex-col absolute top-0 right-0  ">
-//                   {/* nav */}
-//                   {/* divider */}
-//                   <div className="w-full h-1 bg-red-700"></div>
-//                   {/* navigation links */}
-//                   <div className="bg-red-300 p-5">
-//                     <ul className="">{navLinks}</ul>
-//                   </div>
-//                   {/* end footer social media links */}
-//                   <div className="bg-red-200 p-5 flex items-center justify-evenly gap-3">
-//                     {smLinks}
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           ) : (
-//             <div className={interactionIconsStyle}>
-//               <IoMenu />
-//             </div>
-//           )}
-//         </div>
-//         {/* lg */}
-//         <div className="hidden lg:flex lg:flex-col w-full ">
-//           {/* nav */}
-//           {/* divider */}
-//           <div className="w-full h-1 bg-red-700"></div>
-//           {/* navigation links */}
-//           <div className="bg-red-300 p-5">
-//             <ul className="">{navLinks}</ul>
-//           </div>
-//           {/* end footer social media links */}
-//           <div className="bg-red-200 p-5 flex items-center justify-evenly gap-3">
-//             {smLinks}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LeftSideBar;
 import profileImg from "../../../../../public/assets/profile-removebg-preview.png";
 import { Link } from "react-router-dom";
 import { FaAngleDoubleRight, FaGithub } from "react-icons/fa";
@@ -586,19 +347,19 @@ const LeftSideBar = ({ handleSideBar, isSideBarClose }) => {
           {/* nav */}
           {/* divider */}
           <div
-            className={`hidden md:block lg:hidden xl:hidden 2xl:hidden w-full h-1 rounded-t-lg ${
+            className={`hidden sm:hidden md:hidden lg:block xl:block 2xl:block w-full h-1 rounded-t-lg ${
               isSideBarClose ? "bg-red-700" : "bg-red-700"
             }`}>
             {/*  */}
           </div>
           {/* navigation links */}
           {/*  */}
-          <div className="hidden md:block lg:hidden xl:hidden 2xl:hidden">
+          <div className="block sm:block md:block lg:hidden xl:hidden 2xl:hidden">
             <h1>Navigation</h1>
           </div>
           {/*  */}
           <div
-            className={`block md:hidden lg:block xl:block 2xl:block p-0 xl:p-5 ${
+            className={`hidden sm:hidden md:hidden lg:flex xl:flex 2xl:flex p-0 xl:p-5 ${
               isSideBarClose ? "bg-red-300" : "bg-red-300"
             }`}>
             <ul
@@ -612,12 +373,12 @@ const LeftSideBar = ({ handleSideBar, isSideBarClose }) => {
           </div>
           {/* end footer social media links */}
           {/*  */}
-          <div className="hidden md:block lg:hidden xl:hidden 2xl:hidden mx-8">
-            <h1 >Profiles</h1>
+          <div className="block sm:block md:block lg:hidden xl:hidden 2xl:hidden mx-8">
+            <h1>Profiles</h1>
           </div>
           {/*  */}
           <div
-            className={`flex md:hidden lg:block xl:block 2xl:block p-1 md:p-0 lg:p-5 xl:p-5 2xl:p-6 items-center justify-evenly gap-3 rounded-b-lg ${
+            className={`hidden sm:hidden md:hidden lg:flex xl:flex 2xl:flex p-1 md:p-0 lg:p-5 xl:p-5 2xl:p-6 items-center justify-evenly gap-3 rounded-b-lg ${
               isSideBarClose ? "bg-red-200" : "bg-red-200 flex flex-col"
             }`}>
             <ul
@@ -646,21 +407,24 @@ const LeftSideBar = ({ handleSideBar, isSideBarClose }) => {
         gap-1 md:gap-2 lg:gap-3 
         ${isSideBarClose ? "w-full" : "w-full"}`}>
       <div className="w-[50px] md:w-[48px] lg:w-[160px] xl:w-[160px] 2xl:w-[170px] flex justify-center items-center">
-  <img
-    className={`shadow-xl drop-shadow-xl rounded-full 
-      ${isSideBarClose ? "w-[50px] md:w-[48px] lg:w-[160px] xl:w-[160px] 2xl:w-[170px]" 
-                       : "w-[50px] md:w-[48px] lg:w-[56px] xl:w-[60px] 2xl:w-[70px]"}`}
-    src={profileImg}
-    alt="profile image of arif miah"
-  />
-</div>
+        <img
+          className={`shadow-xl drop-shadow-xl rounded-full 
+      ${
+        isSideBarClose
+          ? "w-[50px] md:w-[48px] lg:w-[160px] xl:w-[160px] 2xl:w-[170px]"
+          : "w-[50px] md:w-[48px] lg:w-[56px] xl:w-[60px] 2xl:w-[70px]"
+      }`}
+          src={profileImg}
+          alt="profile image of arif miah"
+        />
+      </div>
 
       {/* settings / users interactions */}
-      <div className="xs:block sm:block md:block lg:hidden xl:hidden 2xl:hidden">
+      <div className="block sm:block md:block lg:hidden xl:hidden 2xl:hidden">
         <h1>Settings</h1>
       </div>
       <div
-        className={`xs:block sm:block md:hidden lg:block xl:block 2xl:block justify-between items-center gap-3 lg:justify-evenly lg:mt-2 ${
+        className={`hidden sm:hidden md:hidden lg:flex xl:flex 2xl:flex justify-between items-center gap-3 lg:justify-evenly lg:mt-2 ${
           isSideBarClose
             ? "flex flex-row gap-12"
             : "flex flex-row md:flex-row lg:flex-col xl:flex-col 2xl:flex-col gap-6"
@@ -862,7 +626,7 @@ const LeftSideBar = ({ handleSideBar, isSideBarClose }) => {
 
       {/* for md */}
       <div className="hidden sm:hidden md:block lg:hidden xl:hidden 2xl:hidden">
-        {/* Content for md devices */}
+        Content for md devices
         {/* footer */}
         {contents}
         {/* {NavForSMDevice} */}
---
+++ src/Version/version3/components/garbageCodeCollection/LeftSideBard.garbage.jsx
@@ -0,0 +1,239 @@
+// import profileImg from "../../../../../public/assets/profile-removebg-preview.png";
+// import { Link } from "react-router-dom";
+// import { FaAngleDoubleRight, FaGithub } from "react-icons/fa";
+// import { FaLinkedin, FaXTwitter } from "react-icons/fa6";
+// import { MdEmail } from "react-icons/md";
+// import { TbFileCv } from "react-icons/tb";
+// import { FaAngleDoubleLeft } from "react-icons/fa";
+// import { useState } from "react";
+// import { GiNightSky } from "react-icons/gi";
+// import { CiBrightnessDown } from "react-icons/ci";
+// import { IoSettings } from "react-icons/io5";
+// import { IoSettingsOutline } from "react-icons/io5";
+// import { CiStar } from "react-icons/ci";
+// import { FaStar } from "react-icons/fa";
+// import { PiEyes } from "react-icons/pi";
+// // import { TbMenuOrder } from "react-icons/tb";
+// import { CgMenuLeft } from "react-icons/cg";
+// import { IoMenu } from "react-icons/io5";
+// // import { SiGmail } from 'react-icons/si';
+// // import Dropdown from '../Dropdown/Dropdown';
+
+// const LeftSideBar = () => {
+//   const [sideBarClose, setSideBarClose] = useState(false);
+//   const [nightModeClose, setNightModeClose] = useState(false);
+//   const [settingModeClose, setSettingModeClose] = useState(false);
+//   const [startClose, setStartClose] = useState(false);
+//   let [countStar, setCountStar] = useState(1);
+//   const [view, setView] = useState(false);
+//   // const [viewEyes, setViewEyes] = useState(10);
+//   const [navbarClose, setNavbarClose] = useState(false);
+
+//   const viewEyes = "1.4k";
+
+//   const handleNavbarTToggle = () => {
+//     // console.log("clicked");
+//     setNavbarClose(!navbarClose);
+//   };
+
+//   const handleView = () => {
+//     // console.log("hovered");
+//     setView(!view);
+//   };
+
+//   const handleStar = () => {
+//     // console.log("hello");
+//     setStartClose(!startClose);
+//     setCountStar(countStar + 1);
+//     if( countStar === 11){
+//       setCountStar(countStar = 0)
+//     }
+//     // console.log(countStar);
+//   };
+
+//   const handleSettingMode = () => {
+//     // console.log("clicked");
+//     setSettingModeClose(!settingModeClose);
+//   };
+
+//   const handleSideBar = () => {
+//     // console.log("clicked");
+//     setSideBarClose(!sideBarClose);
+//   };
+//   const handleNightMode = () => {
+//     console.log("clicked");
+//     setNightModeClose(!nightModeClose);
+//   };
+//   // console.log(viewEyes);
+
+//   const navLinksStyle =
+//     "border-b-2 border-green-800 hover:p-1 hover:bg-green-200 hover:shadow-2xl rounded-md hover:text-center delay-75 hover:transition-shadow transition-all hover:border-x-2 hover:border-green-600 hover:drop-shadow-sm ";
+
+//   const navLinks = (
+//     <>
+//       <li className={navLinksStyle}>
+//         <Link to={"/v3/"}>Home</Link>
+//       </li>
+//       <li className={navLinksStyle}>
+//         <Link to={"/v3/about"}>About</Link>
+//       </li>
+//       <li className={navLinksStyle}>
+//         <Link to={"/v3/skills"}>Skills</Link>
+//       </li>
+//       <li className={navLinksStyle}>
+//         <Link to={"/v3/projects"}>Projects</Link>
+//       </li>
+//       <li className={navLinksStyle}>
+//         <Link to={"/v3/blogs"}>Blogs</Link>
+//       </li>
+//       <li className={navLinksStyle}>
+//         <Link to={"/v3/contact"}>Contact</Link>
+//       </li>
+//     </>
+//   );
+
+//   const smLinksStyle =
+//     "text-2xl hover:text-3xl hover:text-[#384ba7] transition-all duration-300 ease-in-out";
+//   const smLinks = (
+//     <>
+//       <FaGithub className={smLinksStyle} />
+//       <FaLinkedin className={smLinksStyle} />
+//       <MdEmail className={smLinksStyle} />
+//       <FaXTwitter className={smLinksStyle} />
+//       <TbFileCv className={smLinksStyle} />
+//     </>
+//   );
+
+//   const interactionIconsStyle =
+//     "flex items-center justify-center bg-green-50 shadow-xl drop-shadow-xl rounded-full w-10 h-10 ";
+
+//   const applyCustomizationStyle =
+//     "border-b-2 border-green-700 hover:border-b-2 hover:border-green-300 hover:p-2 hover:text-center cursor-pointer hover:bg-blue-700 hover:transition-all hover:rounded-md";
+
+//   return (
+//     <div
+//       className={`bg-[#60A15680] p-1 lg:p-5 flex sm:flex-row sm:justify-between sm:items-center lg:flex-col lg:gap-3  `}>
+//       <div className="flex justify-center items-center   ">
+//         <img
+//           className="shadow-xl drop-shadow-xl rounded-full w-[40px] md:w-[120px] lg:w-[160px] xl:w-[180px] xl:h-[160.28]  "
+//           src={profileImg}
+//           alt="profile image of arif miah"
+//         />
+//       </div>
+//       {/* settings / users interactions */}
+//       <div className="flex flex-row justify-between items-center gap-1 lg:justify-evenly lg:mt-2">
+//         {/* sidebar folding / Drawer */}
+//         <div onClick={handleSideBar} className={interactionIconsStyle}>
+//           {sideBarClose ? <FaAngleDoubleRight /> : <FaAngleDoubleLeft />}
+//         </div>
+//         {/* night mode toggle / theme */}
+//         <div onClick={handleNightMode} className={interactionIconsStyle}>
+//           {nightModeClose ? <CiBrightnessDown /> : <GiNightSky />}
+//         </div>
+//         {/* apply settings */}
+//         <div onClick={handleSettingMode} className={interactionIconsStyle}>
+//           {settingModeClose ? (
+//             <div className="relative">
+//               {" "}
+//               <IoSettings className="" />{" "}
+//               <div className="absolute top-50 right-50 bg-sky-300 p-5 rounded-xl ">
+//                 {" "}
+//                 <h1 className="text-center">Customization Setting</h1>
+//                 <div>
+//                   <ul className="border border-red-400 bg-sky-200 rounded-lg p-1">
+//                     <li className={applyCustomizationStyle}>Font Size</li>
+//                     <li className={applyCustomizationStyle}>Front Family</li>
+//                     <li className={applyCustomizationStyle}>Color</li>
+//                     <li className={applyCustomizationStyle}>Mode</li>
+//                   </ul>
+//                 </div>
+//               </div>
+//             </div>
+//           ) : (
+//             <div>
+//               {" "}
+//               <IoSettingsOutline />{" "}
+//             </div>
+//           )}
+//         </div>
+//         {/* give star  */}
+//         <div
+//           onClick={handleStar}
+//           className={`flex items-center justify-center bg-green-50 shadow-xl drop-shadow-xl rounded-full  w-10 h-10  `}>
+//           {startClose ? (
+//             <FaStar
+//               className={`${
+//                 countStar === 2
+//                   ? "w-10 h-10 text-yellow-300 "
+//                   : countStar === 4
+//                   ? "w-12 h-12 text-pink-600"
+//                   : countStar === 6
+//                   ? "w-14 h-14 text-blue-600"
+//                   : countStar === 8
+//                   ? "w-16 h-16 text-[#26c2a3]"
+//                   : countStar === 10
+//                   ? "w-16 h-16 text-[#26c25d]"
+//                   : ""
+//               }`}
+//             />
+//           ) : (
+//             <CiStar className="" />
+//           )}
+//         </div>
+//         {/* show website view */}
+//         <div
+//           onMouseEnter={handleView}
+//           className={`flex items-center justify-center bg-green-50 shadow-xl drop-shadow-xl rounded-full w-10 h-10 text-[15px] hover:text-[14px] hover:transition-all hover:delay-75 `}>
+//           {view ? viewEyes : <PiEyes />}
+//         </div>
+//       </div>
+//       {/* footer */}
+//       <div>
+//         {/* sm */}
+//         <div onClick={handleNavbarTToggle} className="lg:hidden">
+//           {navbarClose ? (
+//             <div className={interactionIconsStyle}>
+//               <CgMenuLeft />
+
+//               <div className="relative top-6">
+//                 <div className=" flex flex-col absolute top-0 right-0  ">
+//                   {/* nav */}
+//                   {/* divider */}
+//                   <div className="w-full h-1 bg-red-700"></div>
+//                   {/* navigation links */}
+//                   <div className="bg-red-300 p-5">
+//                     <ul className="">{navLinks}</ul>
+//                   </div>
+//                   {/* end footer social media links */}
+//                   <div className="bg-red-200 p-5 flex items-center justify-evenly gap-3">
+//                     {smLinks}
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           ) : (
+//             <div className={interactionIconsStyle}>
+//               <IoMenu />
+//             </div>
+//           )}
+//         </div>
+//         {/* lg */}
+//         <div className="hidden lg:flex lg:flex-col w-full ">
+//           {/* nav */}
+//           {/* divider */}
+//           <div className="w-full h-1 bg-red-700"></div>
+//           {/* navigation links */}
+//           <div className="bg-red-300 p-5">
+//             <ul className="">{navLinks}</ul>
+//           </div>
+//           {/* end footer social media links */}
+//           <div className="bg-red-200 p-5 flex items-center justify-evenly gap-3">
+//             {smLinks}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LeftSideBar;
