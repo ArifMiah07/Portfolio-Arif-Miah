a899df1bbc1cde75a58cb8fc9f831264c74a5b6f
ArifMiah07 <arifmiah.me322@gmail.com>
2025-10-02 21:08:31 +0600

    create a simple static navbar in landing page

--- src/Version/version4/pages/landingPage/LandingPage.jsx
+++ src/Version/version4/pages/landingPage/LandingPage.jsx
@@ -2,6 +2,33 @@ export default function LandingPage() {
   return (
     // this is the landing page
     <div className="w-full  h-full border-4 border-red-500 ">
+      <header className=" w-full h-full flex items-center px-6 py-2 ">
+        <nav className=" w-full h-full flex flex-row items-center justify-between gap-6 ">
+          {/* logo */}
+          <div className="flex flex-row items-center justify-center ">
+            <span>{"<"}</span>
+            <span>Arif Miah</span>
+            <span>{"/>"}</span>
+          </div>
+          {/* menu */}
+          <div className="flex items-center">
+            <ul className="flex flex-row items-center gap-6 ">
+              <li>Home</li>
+              <li>About</li>
+              {/* <li>Skill</li>
+                        <li>Projects</li>
+                        <li>Contact</li> */}
+              <li>Blog</li>
+              <li>Lib</li>
+            </ul>
+          </div>
+          {/* extra */}
+          <div className="flex flex-row items-center justify-center  ">
+            <span>Light</span>
+            <span>Dark</span>
+          </div>
+        </nav>
+      </header>
       <h1 className=" gradientName uppercase text-center font-black text-7xl  ">
         <span>Junior</span>
         <span> Full Stack</span>
