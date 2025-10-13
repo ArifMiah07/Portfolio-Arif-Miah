25c27c36f6abf097415e92c8f543e7afd10eaf20
ArifMiah07 <arifmiah.me322@gmail.com>
2025-10-13 23:26:25 +0600

    working on user profile

--- src/Version/version4/pages/auth/UserProfile.jsx
+++ src/Version/version4/pages/auth/UserProfile.jsx
@@ -1,13 +1,58 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 
-export default function UserProfile(){
+export default function UserProfile() {
+  const { user } = useContext(AuthContext);
 
-    const {user} = useContext(AuthContext) ;
+  if (user !== null) {
+    const userData = user.providerData;
+    console.log("user data: ", userData);
+    console.log("user data: ", userData.displayName);
+    console.log("user data: ", userData.email);
+    console.log("user data: ", userData.phoneNumber);
+    console.log("user data: ", userData.photoURL);
+    console.log("user data: ", userData.providerId);
+    console.log("user data: ", userData.uid);
+  }
+  /***
+   * displayName: "David Meow"
+   * email: "david_meow@gmail.com"
+   * phoneNumber: null photoURL: null
+   * providerId: "password"
+   * uid: "david_meow@gmail.com"
+   *
+   *
+   */
 
-    return(
-        <div className=" w-full min-h-screen  ">
-            Well come back {user?.displayName}
+  return (
+    <div className=" w-full min-h-screen  ">
+      {/* cover image */}
+      <div className="w-full h-full  ">
+        <div className="w-full h-[40vh] overflow-hidden">
+          <img
+            src="https://www.hdwallpapers.in/download/abstract_cover_background_4k_hd-3840x2160.jpg"
+            alt="cover"
+            className="w-full h-full object-cover "
+          />
         </div>
-    )
+      </div>
+      {/* profile info */}
+      <div className="w-full h-full flex flex-col items-center justify-center gap-4  ">
+        <p>
+          Welcome Back Mr.{" "}
+          {user?.displayName ? user.displayName : "name not found"}
+        </p>
+        <p>Email: {user?.email ? user.email : "email not fount"}</p>
+        <p>
+          Profile Image:
+          {user?.photoURL ? user.photoURL : "photo URL not found"}
+        </p>
+        <p>
+          Phone Number:
+          {user?.phoneNumber ? user.phoneNumber : "phone Number not found"}
+        </p>
+      </div>
+      <div>{/* uno */}</div>
+    </div>
+  );
 }
